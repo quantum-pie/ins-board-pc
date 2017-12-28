9fc517a1254a1aa54efd795178230af2c9639b73
Павел Ермаков <ermakov_p@office.kontur-niirs.ru>
2017-12-28 16:16:51 +0300

    Received and parsed UDP packet

--- ins_board_pc/mainwindow.cpp
+++ ins_board_pc/mainwindow.cpp
@@ -1,11 +1,46 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDataStream>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    udp_socket = new QUdpSocket(this);
+    udp_socket->bind(QHostAddress("192.168.4.1"), 65000);
+
+    connect(udp_socket, SIGNAL(readyRead()), this, SLOT(read_datagrams()));
+}
+
+void MainWindow::read_datagrams()
+{
+    while (udp_socket->hasPendingDatagrams())
+    {
+        QNetworkDatagram datagram = udp_socket->receiveDatagram();
+        process_data(datagram.data());
+    }
+}
+
+void MainWindow::process_data(const QByteArray & data)
+{
+    QDataStream ds(data);
+    ds.setByteOrder(QDataStream::LittleEndian);
+    size_t samples = (data.size() - pkt_header_size) / sample_size;
+
+    int pkt;
+    ds >> pkt;
+
+    for(size_t i = 0; i < samples; ++i)
+    {
+        input_t in;
+        ds >> in.et >> in.w_x >> in.w_y >> in.w_z >>
+              in.a_x >> in.a_y >> in.a_z >>
+              in.m_x >> in.m_y >> in.m_z;
+    }
 }
 
 MainWindow::~MainWindow()
--- ins_board_pc/mainwindow.h
+++ ins_board_pc/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,31 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void read_datagrams();
+
 private:
+    void process_data(const QByteArray & data);
+
     Ui::MainWindow *ui;
+    QUdpSocket *udp_socket;
+
+    const size_t pkt_header_size = 4;
+    const size_t sample_size = 76;
+
+    struct input_t
+    {
+        int et;
+        double w_x;
+        double w_y;
+        double w_z;
+        double a_x;
+        double a_y;
+        double a_z;
+        double m_x;
+        double m_y;
+        double m_z;
+    };
 };
 
 #endif // MAINWINDOW_H
