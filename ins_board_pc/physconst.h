762967eeb29ebdde2ec34da5c1fedd9bc2cd6111
Павел Ермаков <ermakov_p@office.kontur-niirs.ru>
2018-02-07 17:20:26 +0300

    Moved physical constants to the separate module

---
+++ ins_board_pc/physconst.h
@@ -0,0 +1,14 @@
+#ifndef PHYSCONST_H
+#define PHYSCONST_H
+
+namespace phconst
+{
+
+const double equator_gravity = 9.7803267714;
+const double standard_gravity = 9.80665;
+const double wgs_k = 0.00193185265241;
+const double wgs_m = 0.00344978650684;
+
+}
+
+#endif // PHYSCONST_H
