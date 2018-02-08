66443f408b52b94e05409c6c02a67bcdedbc2217
Павел Ермаков <ermakov_p@office.kontur-niirs.ru>
2018-02-08 17:01:58 +0300

    Prepared files for position-specific Kalman filter

---
+++ ins_board_pc/poskalman.cpp
@@ -0,0 +1,1 @@
+#include "poskalman.h"
---
+++ ins_board_pc/poskalman.h
@@ -0,0 +1,11 @@
+#ifndef POSKALMAN_H
+#define POSKALMAN_H
+
+#include "abstractpositionfilter.h"
+
+class PositionKalman final : public AbstractPositionFilter
+{
+
+};
+
+#endif // POSKALMAN_H
