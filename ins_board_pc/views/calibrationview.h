6a25ab6c47582c46098d8f09ebf6a936353fcb36
quantum-pie <big.awesome.pie@gmail.com>
2018-05-30 19:49:44 +0300

    Calibration view development

---
+++ ins_board_pc/views/calibrationview.h
@@ -0,0 +1,12 @@
+#ifndef CALIBRATIONVIEW_H
+#define CALIBRATIONVIEW_H
+
+#include "views/IBaseView.h"
+#include "magncalibrator.h"
+
+struct CalibrationView : IBaseView<MagnCalibrator>
+{
+
+};
+
+#endif // CALIBRATIONVIEW_H
