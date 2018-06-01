a3e998cd185851afffa43cdeca9381dfd86564e6
quantum-pie <big.awesome.pie@gmail.com>
2018-06-01 18:19:00 +0300

    Implemented magnetic calibration controller

--- ins_board_pc/controllers/direct/magncalibrationcontroller.cpp
+++ ins_board_pc/controllers/direct/magncalibrationcontroller.cpp
@@ -1,1 +1,35 @@
 #include "magncalibrationcontroller.h"
+#include "receiver.h"
+
+#include <QPushButton>
+
+MagnCalibrationController::MagnCalibrationController(MagnCalibrator & calibrator, const Receiver *receiver, const QPushButton *calibrate_btn)
+    : RunningFlag{ calibrate_btn->isChecked() }, calibration_model{ calibrator }
+{
+    connect(receiver, SIGNAL(raw_packet_received(RawPacket)), this, SLOT(handle_input(RawPacket)));
+    connect(calibrate_btn, SIGNAL(toggled(bool)), this, SLOT(handle_calibrate(bool)));
+}
+
+void MagnCalibrationController::handle_input(const RawPacket & z)
+{
+    if(is_running())
+    {
+        calibration_model.get().update(z.m);
+    }
+}
+
+void MagnCalibrationController::handle_calibrate(bool en)
+{
+    set_running(en);
+    if(en)
+    {
+        clear_views();
+    }
+    else
+    {
+        calibration_model.get().fit();
+        update_views(calibration_model);
+    }
+
+    calibration_model.get().clear_meas();
+}
--- ins_board_pc/controllers/direct/magncalibrationcontroller.h
+++ ins_board_pc/controllers/direct/magncalibrationcontroller.h
@@ -1,4 +1,37 @@
 #ifndef MAGNCALIBRATIONCONTROLLER_H
 #define MAGNCALIBRATIONCONTROLLER_H
 
+#include "views/IBaseView.h"
+#include "controllers/direct/observablebase.h"
+#include "controllers/direct/runningflag.h"
+#include "magncalibrator.h"
+#include "packets.h"
+
+#include <QObject>
+
+#include <functional>
+
+class Receiver;
+class QPushButton;
+
+class MagnCalibrationController : public QObject,
+                                  public ObservableBase<ICalibrationView>,
+                                  RunningFlag
+{
+    Q_OBJECT
+
+public:
+    MagnCalibrationController(MagnCalibrator & calibrator, const Receiver * receiver, const QPushButton * calibrate_btn);
+
+    using RunningFlag::is_running;
+    using RunningFlag::set_running;
+
+public slots:
+    void handle_input(const RawPacket & z);
+    void handle_calibrate(bool en);
+
+private:
+    std::reference_wrapper<MagnCalibrator> calibration_model;
+};
+
 #endif // MAGNCALIBRATIONCONTROLLER_H
