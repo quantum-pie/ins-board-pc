f5573c6535d53cce4e45f8cb45d3e57d1fbdd59d
quantum-pie <big.awesome.pie@gmail.com>
2018-06-04 13:59:33 +0300

    Implemented flexible UDP receiver with injectable behaviour

--- ins_board_pc/udpreceiver.cpp
+++ ins_board_pc/udpreceiver.cpp
@@ -1,7 +1,17 @@
 #include "udpreceiver.h"
 
 UDPReceiver::UDPReceiver(const QString &ip, uint16_t port, processor_type proc)
+    : processor{ proc }
 {
     sock.bind(QHostAddress(ip), port);
-    connect(&raw_socket, SIGNAL(readyRead()), this, SLOT(read_raw_datagrams()));
+    connect(&sock, SIGNAL(readyRead()), this, SLOT(read_datagrams()));
+}
+
+void UDPReceiver::read_datagrams()
+{
+    while(sock.hasPendingDatagrams())
+    {
+        QNetworkDatagram datagram = sock.receiveDatagram();
+        processor(datagram.data());
+    }
 }
--- ins_board_pc/udpreceiver.h
+++ ins_board_pc/udpreceiver.h
@@ -2,18 +2,25 @@
 #define UDPRECEIVER_H
 
 #include <QtNetwork>
+#include <QObject>
 
 #include <functional>
 
-class UDPReceiver
+class UDPReceiver : public QObject
 {
+    Q_OBJECT
+
 public:
     using processor_type = std::function<void(const QByteArray & data)>;
 
     UDPReceiver(const QString & ip, uint16_t port, processor_type proc);
 
+public slots:
+    void read_datagrams();
+
 private:
     QUdpSocket sock;
+    processor_type processor;
 };
 
 #endif // UDPRECEIVER_H
