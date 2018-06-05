e1d6ea4220c11a1705f888de9470a64f23b487c6
quantum-pie <big.awesome.pie@gmail.com>
2018-06-05 14:18:34 +0300

    Fixed bug in filtering controller

--- ins_board_pc/controllers/direct/filtering/filteringcontroller.h
+++ ins_board_pc/controllers/direct/filtering/filteringcontroller.h
@@ -10,19 +10,13 @@
 
 template<typename Model, typename View>
 struct FilteringController : ControllerBase<Model>, ObservableBase<View>,
-                             private FilteringControllerCommon
+                             FilteringControllerCommon
 {
     FilteringController(const QPushButton * start_button)
         : FilteringControllerCommon{ start_button->isChecked() }
     {
         connect(start_button, SIGNAL(toggled(bool)), this, SLOT(handle_start(bool)));
     }
-
-    using FilteringControllerCommon::is_running;
-    using FilteringControllerCommon::set_running;
-    using FilteringControllerCommon::filtering_is_enabled;
-    using FilteringControllerCommon::enable_filtering;
-    using FilteringControllerCommon::disable_filtering;
 
     void handle_start(bool en)
     {
